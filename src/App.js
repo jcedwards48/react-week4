35902b72352ab9448df82e1ccc23dd1d69f31f01
jcedwards48 <105121272+jcedwards48@users.noreply.github.com>
2022-10-12 13:35:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather Engine</h1>
-      <footer>
-        This project was coded by Jeanne Edwards and is open-sourced on Github
-        <a
-          href="https://github.com/jcedwards48/react-week4"
-          target="_blank"
-        ></a>
-        Search />
-      </footer>
+      Search />
     </div>
   );
 }

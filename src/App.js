eac1310e9e8d1bed68aaac586d8557e760f6fbf8
jcedwards48 <105121272+jcedwards48@users.noreply.github.com>
2022-10-12 13:02:37 -0400

    Update App.js
    Sdded link to GH for Netlify push

--- src/App.js
+++ src/App.js
@@ -5,31 +5,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather Engine</h1>
-      <Search />
+      <footer>
+        This project was coded by Jeanne Edwards and is open-sourced on Github
+        <a
+          href="https://github.com/jcedwards48/react-week4"
+          target="_blank"
+        ></a>
+        <Search />
+      </footer>
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
